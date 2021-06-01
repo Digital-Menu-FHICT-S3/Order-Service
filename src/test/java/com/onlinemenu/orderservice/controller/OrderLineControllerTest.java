06e29ebeb2760347394023e2834c06360f4fdb94
Piet2001 <piet2001@fireman.net>
2021-06-01 15:51:02 +0200

    remove some unused imports

--- src/test/java/com/onlinemenu/orderservice/controller/OrderLineControllerTest.java
+++ src/test/java/com/onlinemenu/orderservice/controller/OrderLineControllerTest.java
@@ -13,7 +13,6 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import javax.transaction.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
