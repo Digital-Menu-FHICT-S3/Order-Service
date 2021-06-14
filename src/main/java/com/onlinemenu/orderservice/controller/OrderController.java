de2a3f5dc18e242e13b82f8eaa14d80eeacdc477
Milan <58393068+Desoxyr@users.noreply.github.com>
2021-06-14 10:24:32 +0200

    Started fetching ingredients and subtracting

--- src/main/java/com/onlinemenu/orderservice/controller/OrderController.java
+++ src/main/java/com/onlinemenu/orderservice/controller/OrderController.java
@@ -1,5 +1,6 @@
 package com.onlinemenu.orderservice.controller;
 
+import com.onlinemenu.orderservice.VO.Dish;
 import com.onlinemenu.orderservice.dto.FoodOrderDto;
 import com.onlinemenu.orderservice.dto.OrderLineDto;
 import com.onlinemenu.orderservice.entity.FoodOrder;
@@ -7,7 +8,9 @@ import com.onlinemenu.orderservice.entity.OrderLine;
 import com.onlinemenu.orderservice.service.OrderLineService;
 import com.onlinemenu.orderservice.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,11 +26,20 @@ public class OrderController {
     @Autowired
     private OrderLineService orderLineService;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @PostMapping("/create")
     public FoodOrder saveOrder(@RequestBody FoodOrderDto foodOrderDto){
 
         if (foodOrderDto.getOrderLines() == null || foodOrderDto.getOrderLines().size() == 0)
              return new FoodOrder();
+
+        //Call GetAllIngredients from Menu Service To Get Ingredients of Dishes
+        String allIngredientUrl = "http://locahost:9191/menu/dishes/dishes-with-ingredients";
+        Dish[] dishesWithIngredients = restTemplate.getForEntity(allIngredientUrl, Dish[].class).getBody();
+
+        //TODO: Check if there's enough stock here and return error if not
 
         //Save FoodOrder
         FoodOrder foodOrder = orderService.saveOrder(foodOrderDto.getFoodOrder());
@@ -38,7 +50,16 @@ public class OrderController {
             orderLines.add(new OrderLine(foodOrder.getOrderId(),dto.getDishId(),dto.getAmount()));
         }
         orderLineService.saveOrderLines(orderLines);
-        
+
+        //TODO Subtract ingredients from stock
+        // Have List<Dish> (which includes ingredients)
+
+        //Call SubtractIngredients from Menu Service.
+        String subtractIngredientsUrl = "http://locahost:9191/menu/dishes/dishes-with-ingredients";
+       restTemplate.postForEntity(allIngredientUrl, Dish[].class).getBody();
+
+
+
         return foodOrder;
     }
 
